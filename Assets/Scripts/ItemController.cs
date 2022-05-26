f02081c6d429d04c597fb31f76113f648077aaca
Irene <ivinentmercadal@iesjoanramis.org>
2022-05-26 11:48:44 +0200

    Millora escena 1: afegir collectibles, traps, estats player, cameraFollower, water animation

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void DestroyItem() {
+        Destroy(this.gameObject);
+    }
+}
